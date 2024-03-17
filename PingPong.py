b53d16b553ec6553be0d73fe250bf4d8a749a342
Rokit8481 <162879025+Rokit8481@users.noreply.github.com>
2024-03-17 12:48:55 +0200

    Класи
    Добавлено класи GameSprite та Player

--- PingPong.py
+++ PingPong.py
@@ -4,6 +4,30 @@ window = display.set_mode((500, 700))
 display.set_caption('Пінг-Понг')
 background = transform.scale(image.load("sky.jpg"), (500, 700))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+ 
+        self.image = transform.scale(
+            image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+            
 clock = time.Clock()
 FPS = 60
 
